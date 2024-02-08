35414214d49e4ff3320b4f15afadf6fa1a138e4f
MariaFD00 <mariafuentes.alum@insestatut.cat>
2024-02-08 19:32:49 +0100

    canvi

--- operaciones/index.js
+++ operaciones/index.js
@@ -18,7 +18,13 @@ function suma(...valors) {
  * @param  {...Number} valors els valors amb els que realitzar la operacio
  * @returns {Number} el resultat de la operacio
  */
-function multiplica(...valors) {}
+function multiplica(...valors) {
+    let total = valors[0];
+    for (let i = 1; i < valors.length; i++) {
+        total *= valors[i];
+    }
+    return total;
+}
 
 module.exports.suma = suma;
 module.exports.multiplica = multiplica;
